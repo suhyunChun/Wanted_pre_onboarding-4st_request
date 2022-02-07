b7daf5922cbf31085296451e5a6ca65222ff27a8
choi2601 <byeonghyeonchoe55@gmail.com>
2022-02-08 02:26:12 +0900

    Add: InfoListTemplate & InfoListForm style 적용

--- src/pages/main/InfoListForm.tsx
+++ src/pages/main/InfoListForm.tsx
@@ -8,16 +8,49 @@ import type { InfoType } from '../../types/components/infolist';
 const InfoListForm: FC<OptionalProps> = ({ method, material, status }) => {
   const { sortedInfoList } = useInfoListState({ method, material, status });
 
+  const checkEmptyList = (): boolean => {
+    let isEmpty = true;
+
+    const currentInfoListNum = sortedInfoList.length;
+
+    if (currentInfoListNum === 0) isEmpty = true;
+
+    return isEmpty;
+  };
+
   return (
     <Container>
-      {sortedInfoList.length !== 0 &&
+      {checkEmptyList() ? (
+        <GuideLine>
+          <FrontInformation>조건에 맞는 견적 요청이 없습니다.</FrontInformation>
+        </GuideLine>
+      ) : (
+        sortedInfoList.length !== 0 &&
         sortedInfoList.map((info: InfoType) => {
           return <CardBoard key={info.id} {...info} />;
-        })}
+        })
+      )}
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  max-width: 1130px;
+`;
+
+const GuideLine = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 38px 0;
+  text-align: center;
+  border: 1px solid #c2c2c2;
+  border-radius: 4px;
+`;
+
+const FrontInformation = styled.div`
+  color: #939fa5;
+  font-size: 14px;
+  line-height: 20px;
+`;
 
 export default InfoListForm;
--- src/pages/main/InfoListTemplate.tsx
+++ src/pages/main/InfoListTemplate.tsx
@@ -1,14 +1,13 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-const InfoListTemplate: FC = ({children}) => {
-    return (
-        <Container>
-            {children}
-        </Container>
-    )
-}
+const InfoListTemplate: FC = ({ children }) => {
+  return <Container>{children}</Container>;
+};
 
-const Container = styled.div``;
+const Container = styled.div`
+  max-width: 1130px;
+  margin: 0 auto;
+`;
 
 export default InfoListTemplate;

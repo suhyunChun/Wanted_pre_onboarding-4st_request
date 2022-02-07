05135f7879f0359bab4bdd18b9d005ed814a8c61
pomeranian91 <forking91@gmail.com>
2022-02-07 23:02:28 +0900

    Add:레이아웃

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/nav/Nav';
-import Container from './components/Container';
 import MainPage from './pages/main/MainPage';
 
 const Router: FC = () => {
